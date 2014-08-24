7f065fd2448809c8c73d50255339888ee44a9425
17771258 <17771258@qq.com>
2014-08-24 09:25:55 +0800

    添加权限认证测试

--- apps/demo/controller/auth.php
+++ apps/demo/controller/auth.php
@@ -2,7 +2,16 @@
 class demo_ctl_auth extends controller{
     public function run(){
         $auth = new auth();
-        $d = $auth->check('admin-index-can', 1);
+        P($auth);
+        $d = $auth->check('admin-index-can,admin-index-run', 1, 1, 'url', 'or');
+        $d2 = $auth->check('admin-index-can,admin-index-run', 1, 1, 'url', 'and');
         P($d);
+        is_administrator();
+        P($d2);
+        $demo = array('1','2',3,4,5,6);
+        debug('21321313132');
+        debug($demo);
+        debug(I('session.'));
+        $this->display();
     }
 }
