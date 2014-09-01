abd2bf044130be94c1ac6274989535fb10194a76
17771258 <17771258@qq.com>
2014-09-01 20:56:28 +0800

    调整base分组不再次加载function.php

--- apps/base/lib/core/app.php
+++ apps/base/lib/core/app.php
@@ -10,7 +10,9 @@ class app {
         if(is_file($confFile))      C(include $confFile);            // 加载当前分组配置
         if(is_file($hookFile))      C('self', include $hookFile);    // 加载当前分组钩子
         if(is_file($langFile))      L(include $langFile);            // 加载当前分组语言包
-        if(is_file($funcFile))      require_cache($funcFile);        // 加载当前分组函数
+        if(G_NAME != 'base'){
+            if(is_file($funcFile))      require_cache($funcFile);    // 加载当前分组函数
+        }
         define('NOW_TIME',          $_SERVER['REQUEST_TIME']);
         define('REQUEST_METHOD',    $_SERVER['REQUEST_METHOD']);
         define('IS_GET',            REQUEST_METHOD =='GET' ? true : false);
