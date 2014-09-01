61bb91b84cde042c341da02e0661794febcceaac
17771258 <17771258@qq.com>
2014-09-01 21:01:29 +0800

    添加安装home默认文件

---
+++ apps/base/view/indexController.php
@@ -0,0 +1,6 @@
+<?php
+    class home_ctl_index extends controller{
+        public function run(){
+            $this->display();
+        }
+    }
---
+++ apps/base/view/indexView.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang=\"zh-cn\">
+<head>
+    <meta charset=\"UTF-8\">
+    <title>EZOS欢迎界面</title>
+</head>
+<body>
+    <h1>欢迎使用EZOS</h1>
+    <p>当前为Home分组下的index控制器run方法</p>
+</body>
+</html>
