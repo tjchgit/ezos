5996b524752ff7e65490183e199530ee05a60956
17771258 <17771258@qq.com>
2014-09-06 00:38:22 +0800

    更新U方法支持更重URL模式

--- apps/base/lib/core/url.php
+++ apps/base/lib/core/url.php
@@ -21,22 +21,24 @@ class url {
                 $array  = explode('/',$rule);
                 $group = array_shift($array);
                 if(!empty($group)) {
-                    $_GET[C('VAR_GROUP')]  =   $group;
+                    $_GET[C('VAR_MODULE')]  =   $group;
                 }
                 if(!empty($array)) {
-                    $_GET[C('VAR_MODULE')]   =   array_shift($array);
+                    $_GET[C('VAR_CONTROLLER')]   =   array_shift($array);
                 }
             }
         }
         $args = self::formatUrl();
         $var = array();
-        if(!isset($_GET[C('VAR_GROUP')])) {
-            $var[C('VAR_GROUP')] = (isset($args[0]) && is_dir(APP_DIR.$args[0])) ? array_shift($args) : C('DEFAULT_GROUP');
-        }
         if(!isset($_GET[C('VAR_MODULE')])) {
-            $var[C('VAR_MODULE')] = array_shift($args);
+            $var[C('VAR_MODULE')] = (isset($args[0]) && is_dir(APP_DIR.$args[0])) ? array_shift($args) : C('DEFAULT_MODULE');
         }
-        $var[C('VAR_ACTION')] = array_shift($args);
+        if(!isset($_GET[C('VAR_CONTROLLER')])) {
+            $var[C('VAR_CONTROLLER')] = array_shift($args);
+        }
+        if(!isset($_GET[C('VAR_ACTION')])){
+            $var[C('VAR_ACTION')] = array_shift($args);
+        }
         if (!empty($args)) {
             $count = count($args);
             for ($i = 0; $i < $count;) {
@@ -44,10 +46,8 @@ class url {
                 $i += 2;
             }
         }
-        $_GET = array_merge($_GET, $var);
-        if(C('URL_MODEL') == URL_COMPAT){
-            unset($_GET[C('VAR_PATHINFO')]);
-        }
+        $_GET = array_merge($var, $_GET);
+        if(C('URL_MODEL') == URL_COMPAT){ unset($_GET[C('VAR_PATHINFO')]);}
         self::setConst();
     }
 
@@ -60,35 +60,33 @@ class url {
         defined('__WEB__') or define("__WEB__", __HOST__ . $_SERVER['SCRIPT_NAME']);
         defined('__URL__') or define("__URL__", __HOST__ . '/' . trim($_SERVER['REQUEST_URI'],'/'));
         define("__HISTORY__", isset($_SERVER["HTTP_REFERER"])?$_SERVER["HTTP_REFERER"]:null);
-        define('G_NAME',    self::getName(C('VAR_GROUP'),   C('DEFAULT_GROUP')));
-        define('M_NAME',    self::getName(C('VAR_MODULE'),  C('DEFAULT_MODEL')));
+        define('M_NAME',    self::getName(C('VAR_MODULE'),   C('DEFAULT_MODULE')));
+        define('C_NAME',    self::getName(C('VAR_CONTROLLER'),  C('DEFAULT_CONTROLLER')));
         define('A_NAME',    self::getName(C('VAR_ACTION'),  C('DEFAULT_ACTION')));
-        define('G_DIR',     APP_DIR.G_NAME.'/');
-        define('G_PATH',    APP_PATH.G_NAME.'/');
-        define('M_DIR',     G_DIR.'model/');
-        define('V_DIR',     G_DIR.'view/');
-        define('C_DIR',     G_DIR.'controller/');
+        define('M_DIR',     APP_DIR.M_NAME.'/');
+        define('M_PATH',    APP_PATH.M_NAME.'/');
+        define('V_DIR',     M_DIR.'view/');
+        define('C_DIR',     M_DIR.'controller/');
         return ;
     }
 
     static public function formatUrl(){
         $urlModel = C('URL_MODEL');
         if($urlModel== URL_COMPAT && isset($_GET[C('VAR_PATHINFO')])){
-            $query = $_GET[C("PATHINFO_VAR")];  // 兼容模式
-        }elseif ($urlModel == URL_REWRITE && isset($_SERVER['PATH_INFO'])){
+            $query = $_GET[C("VAR_PATHINFO")];  // 兼容模式
+        }else if(($urlModel == URL_PATHINFO || $urlModel == URL_REWRITE) && isset($_SERVER['PATH_INFO'])){
             $query = $_SERVER['PATH_INFO'];     // PATHINFO
         }else{
             $query = $_SERVER['QUERY_STRING'];  // 其它模式
         }
         $url = self::parseRoute(str_ireplace(C('URL_HTML_SUFFIX'), '', trim($query, '/')));
         $gets = '';
-        if($urlModel == URL_REWRITE || $urlModel == URL_COMPAT && isset($_GET[C('VAR_PATHINFO')])){
+        if($urlModel == URL_REWRITE || $urlModel == URL_PATHINFO || $urlModel == URL_COMPAT && isset($_GET[C('VAR_PATHINFO')])){
             $url = str_replace(array('&', '='), C("URL_PATHINFO_DEPR"), $url);
         }else{
             parse_str($url, $gets);
             $_GET = array_merge($_GET, $gets);
         }
-
         return $gets || empty($url) ? array() : explode(C("URL_PATHINFO_DEPR"), $url);
     }
 
@@ -122,15 +120,7 @@ class url {
 
     static public function tourl($url){
         $route = C("ROUTE");
-        fb($route);
-        fb($route);
-        fb($route);
-        fb($route);
-        fb($route);
-        fb($route);
-        fb($route);
-        fb($route);
-        if(!$route) return $url ;
+        if(!$route || C('URL_MODEL') != URL_REWRITE) return $url ;
         static $_route = array();
         $oldUrl = $url;
         if(isset($_route[$oldUrl])) return $_route[$oldUrl];
