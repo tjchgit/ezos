080ac08d4403607e7b83fb7744d5445fa5ac1e4c
17771258 <17771258@qq.com>
2014-09-21 20:09:57 +0800

    修改模版引擎

--- apps/base/hooks/parse.php
+++ apps/base/hooks/parse.php
@@ -1,15 +1,31 @@
 <?php
 class base_hok_parse extends hook {
     public function run(&$_data) {
-        $engine             = strtolower( C('TMPL_ENGINE_TYPE') );
         $_content           = empty($_data['content']) ? $_data['file'] : $_data['contet'];
         $_data['prefix']    = empty($_data['prefix']) ? C('TMPL_CACHE_PREFIX') : $_data['prefix'] ;
-        $class              = 'base_driver_tpl_'.$engine;
-        if(class_exists($class)) {
-            $tpl    = new $class;
-            $tpl -> fetch($_content, $_data['var']);
-        }else{
-            E("系统不支持：".$class);
-        }
+        $tpl = new template();
+        $tpl->fetch($_content,$_data['var'],$_data['prefix']);
+    }
+
+    /**
+     * 检查缓存文件是否有效
+     * 如果无效则需要重新编译
+     * @access protected
+     * @param string $tmplTemplateFile  模板文件名
+     * @return boolean
+     **/
+    protected function checkCache($tmplTemplateFile ){
+
+    }
+
+    /**
+     * 检查缓存内容是否有效
+     * 如果无效则需要重新编译
+     * @access protected
+     * @param string $tmplContent  模板内容
+     * @return boolean
+     **/
+    protected function checkContentCache($tmplContent){
+
     }
 }
